fc18deecb702ecb32ca0372be701b2e7313aa185
Yariv Matia <yariv.matia@gmail.com>
2026-05-27 13:15:02 +0200

    attempt 3

--- ex_app/src/main.js
+++ ex_app/src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import { translate, translatePlural } from '@nextcloud/l10n'
+import { generateUrl } from '@nextcloud/router'
+import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './AppAPI.js'
+import { getCSPNonce } from '@nextcloud/auth'
+
+Vue.prototype.t = translate
+Vue.prototype.n = translatePlural
+Vue.prototype.OC = window.OC
+Vue.prototype.OCA = window.OCA
+
+__webpack_public_path__ = generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/js/`) // eslint-disable-line
+__webpack_nonce__ = getCSPNonce() // eslint-disable-line
 
 const app = createApp(App)
 app.mount('#expense_report')
