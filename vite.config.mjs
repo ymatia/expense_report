7692ddba53113bb6e59e7626874c9fa44eda929b
Yariv Matia <yariv.matia@gmail.com>
2026-05-28 14:34:02 +0200

    modify vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { createAppConfig } from '@nextcloud/vite-config'
+import { join, resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'src',
-    emptyOutDir: false,
-    rollupOptions: {
-      input: resolve(__dirname, 'src/main.ts'),
-      output: {
-        entryFileNames: 'main.js',
-        format: 'iife'
-      }
-    }
-  }
-})
+export default createAppConfig(
+	{
+		main: resolve(join('src', 'main.js')),
+	},
+	{
+		createEmptyCSSEntryPoints: true,
+		extractLicenseInformation: true,
+		thirdPartyLicense: false,
+	},
+)
