e7fb06497f47c3b77c57fc28db5b4bd75b5507c7
Yariv Matia <yariv.matia@gmail.com>
2026-06-10 10:34:55 +0200

    export defaults

--- src/DetailedView.vue
+++ src/DetailedView.vue
@@ -8,17 +8,16 @@
 	import axios from '@nextcloud/axios'
 	import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './AppAPI.js'
 
-	new Vue({
-	  el: '#DetailedView',
-	  data () {
-		return {
-		  info: null
+	export default {
+		data () {
+			return {
+				info: null
+			}
+		},
+		mounted () {
+			axios
+				.get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data`))
+				.then(response => (this.info = response))
 		}
-	  },
-	  mounted () {
-		axios
-		  .get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data`))
-		  .then(response => (this.info = response))
-	  }
-	})
+	}
 </script>
