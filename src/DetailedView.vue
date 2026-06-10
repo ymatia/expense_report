9f643fe41a66a0880ca8575f2618c894a6b68f56
Yariv Matia <yariv.matia@gmail.com>
2026-06-10 10:26:41 +0200

    adding data

--- src/DetailedView.vue
+++ src/DetailedView.vue
@@ -1,6 +1,24 @@
 <template>
-	DetailedView
+	<div id="DetailedView">
+		{{ info }}
+	</div>
 </template>
 
 <script>
+	import axios from '@nextcloud/axios'
+	import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './AppAPI.js'
+
+	new Vue({
+	  el: '#DetailedView',
+	  data () {
+		return {
+		  info: null
+		}
+	  },
+	  mounted () {
+		axios
+		  .get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data`))
+		  .then(response => (this.info = response))
+	  }
+	})
 </script>
