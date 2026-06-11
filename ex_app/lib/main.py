b20964d22b26d809800f7242c9c2fa0d38e0f8eb
Yariv Matia <yariv.matia@gmail.com>
2026-06-11 12:31:06 +0200

    use the cookies

--- ex_app/lib/main.py
+++ ex_app/lib/main.py
@@ -419,7 +419,10 @@ APP.mount("/js", StaticFiles(directory="../js"), name="js")
 async def report_data(request: Request, year: int | None = None):
     global token
     print("in data")
-    token = request.headers.get("requesttoken")
+    cookies = request.headers.get("Cookie")
+    cookie_arr = cookies.split("; ")
+    cookie_dict = {x.split("=")[0]:x.split("=")[1] for x in cookies_arr}
+    token = cookie_dict["nc_token"]
     print(f"token={token}")
     report_year = year or datetime.today().year
     try:
