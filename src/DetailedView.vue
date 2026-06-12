1a2565926b583cd861bb0f1078a34dc5123047d5
Yariv Matia <yariv.matia@gmail.com>
2026-06-12 17:56:35 +0200

    one more try

--- src/DetailedView.vue
+++ src/DetailedView.vue
@@ -5,10 +5,11 @@
 </template>
 
 <script>
-	import axios from '@nextcloud/axios'
-	import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './AppAPI.js'
-	import { generateUrl } from '@nextcloud/router'
-	import { EasyDataTable } from Vue3EasyDataTable
+	import axios from '@nextcloud/axios';
+	import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './AppAPI.js';
+	import { generateUrl } from '@nextcloud/router';
+	import EasyDataTable from Vue3EasyDataTable;
+	import "vue3-easy-data-table/dist/style.css";
 
 	export default {
 		components: {
