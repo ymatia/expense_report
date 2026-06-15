2fc4c5b7af4803308377f4c33240acc7f9ac2ef6
Yariv Matia <yariv.matia@gmail.com>
2026-06-15 13:55:30 +0200

    another attempt

--- src/DetailedView.vue
+++ src/DetailedView.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="DetailedView">
-		<EasyDataTable :headers="jsonData.headers" :items="jsonData.data" />
+		<EasyDataTable :headers="headers" :items="items" />
 	</div>
 </template>
 
@@ -11,16 +11,14 @@
 	import EasyDataTable from "vue3-easy-data-table";
 	import "vue3-easy-data-table/dist/style.css";
 
-	var jsonData = {};
-	var str = "";
-
 	export default {
 		components: {
 			EasyDataTable
 		},
 		data () {
 			return {
-				jsonData: { headers: [], data: [] }
+				headers:[],
+				data:[]
 			}
 		},
 		created () {
@@ -28,7 +26,9 @@
 				.get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data`))
 				.then(function (response) {
 					console.log(response.data);
-					this.jsonData = JSON.parse(response.data)
+					var jsonData = JSON.parse(response.data);
+					this.headers = jsonData.headers;
+					this.data = jsonData.data;
 				})
 		}
 	}
