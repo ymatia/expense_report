253f61e83de1fbf8f9f3c3abf1ca6d3eec062512
Yariv Matia <yariv.matia@gmail.com>
2026-06-26 11:56:33 +0200

    more fixes

--- src/ReportView.vue
+++ src/ReportView.vue
@@ -32,7 +32,7 @@
 		methods: {
 			loadData: function() {
 				axios
-					.get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data?reportName=${route.params.id}`))
+					.get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data?reportName=${this.$route.params.reportName}`))
 					.then((response) => {
 						console.log(response.data);
 						var jsonData = JSON.parse(response.data);
--- src/git add commit and push.cmd
+++ src/git add commit and push.cmd
@@ -4,5 +4,3 @@ cd C:\Code\expense_report
 git add *
 git commit -m "%CommentVar%"
 git push
-pause
-ssh ymatia@192.168.0.28 ./redeploy_expense_report.sh
