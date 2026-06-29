4b2ac4a1c9781f40e87ce53e9e95f988a0dc132d
Yariv Matia <yariv.matia@gmail.com>
2026-06-29 11:41:27 +0200

    removed router

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,44 @@
 		<NcAppNavigation>
 			<NcAppNavigationCaption heading-id="navigation-heading" is-heading name="Reports"/>
 			<template #list>
-				<NcAppNavigationItem name="Finance Overview" to="/report/monthly">
+				<NcAppNavigationItem name="Finance Overview" @click="loadData('monthly')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Finance by Category" to="/report/category">
+				<NcAppNavigationItem name="Finance by Category" @click="loadData('category')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Cash Flow 00" to="/report/cash_flow">
+				<NcAppNavigationItem name="Cash Flow 00" @click="loadData('cash_flow')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Cash Flow 01" to="/report/cash_flow_01">
+				<NcAppNavigationItem name="Cash Flow 01" @click="loadData('cash_flow_01')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Finance Overview Previous Year" to="/report/monthly_PrevYear">
+				<NcAppNavigationItem name="Finance Overview Previous Year" @click="loadData('monthly_PrevYear')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Debts" to="/report/debts">
+				<NcAppNavigationItem name="Debts" @click="loadData('debts')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Debts Summary" to="/report/debts_summary">
+				<NcAppNavigationItem name="Debts Summary" @click="loadData('debts_summary')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
-				<NcAppNavigationItem name="Reel Usage" to="/report/reel_usage">
+				<NcAppNavigationItem name="Reel Usage" @click="loadData('reel_usage')">
 					<template #icon><svg-icon type="mdi" :path="mdiCheckPath"></svg-icon></template>
 				</NcAppNavigationItem>
 			</template>
 		</NcAppNavigation>
 		<NcAppContent>
-			<router-view />
+			<div id="ReportView" class="main-div">
+				<EasyDataTable 
+					:headers="headers" 
+					:items="items" 
+					theme-color="#1d90ff"
+					table-class-name="customize-table"
+					:loading="loading"
+					alternating 
+					body-text-direction="right"
+				/>
+			</div>
 		</NcAppContent>
 	</NcContent>
 </template>
@@ -48,6 +58,12 @@
 	// Icons
 	import SvgIcon from '@jamescoyle/vue-icon';
 	import { mdiCheck } from '@mdi/js';
+	import axios from '@nextcloud/axios';
+	import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './AppAPI.js';
+	import { generateUrl } from '@nextcloud/router';
+	import EasyDataTable from "vue3-easy-data-table";
+	import "vue3-easy-data-table/dist/style.css";
+	import "./MyStyle.css";
 
 	export default {
 		components: {
@@ -55,11 +71,27 @@
 			NcAppNavigation, 
 			NcAppNavigationItem,
 			NcAppContent, 
-			SvgIcon
+			SvgIcon,
+			EasyDataTable
 		},
 		data() {
 			return {
 				mdiCheckPath: mdiCheck,
+				headers: [],
+				items: [],
+				loading: true
+			}
+		},
+		methods: {
+			loadData: function(reportName) {
+				axios
+					.get(generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/data?reportName=${reportName}`))
+					.then((response) => {
+						var jsonData = JSON.parse(response.data);
+						this.headers = jsonData["headers"];
+						this.items = jsonData["items"];
+						this.loading = false;
+				})
 			}
 		}
 	}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Vue-Router
-import { createMemoryHistory, createRouter } from 'vue-router'
-
 import FinanceOverviewView from './FinanceOverviewView.vue'
 import DetailedView from './DetailedView.vue'
 import ReportView from './ReportView.vue'
@@ -11,16 +8,6 @@ import ReportView from './ReportView.vue'
 import Vue3EasyDataTable from 'vue3-easy-data-table'
 import 'vue3-easy-data-table/dist/style.css';
 
-const routes = [
-  { path: '/report/:reportName', component: ReportView }
-]
-
-export const router = createRouter({
-  history: createMemoryHistory(),
-  routes,
-})
-
 const app = createApp(App)
-app.use(router)
 app.component('EasyDataTable', Vue3EasyDataTable)
 app.mount('#content')
